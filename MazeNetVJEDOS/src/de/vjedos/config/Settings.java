582fbbfcdd22fb53c6f646055df8733d92c0f5f6
VJEDOS <vethiha@gmail.com>
2015-05-27 14:05:31 +0200

    initial commit

---
+++ MazeNetVJEDOS/src/de/vjedos/config/Settings.java
@@ -0,0 +1,7 @@
+package de.vjedos.config;
+
+public class Settings {
+	
+	public static final int PORT =5123;
+
+}
---
+++ MazeNetVJEDOS/src/de/vjedos/main/Main.java
@@ -0,0 +1,12 @@
+package de.vjedos.main;
+
+import java.net.Socket;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+	Socket s = new Socket();
+
+	}
+}
